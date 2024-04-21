8376eb5dacbb537a72429cdbf5a576f9882cfb2e
ruancrdz <ruan.montanari@sptech.school>
2024-04-21 20:22:00 -0300

    Aula 3

--- js/buscar.js
+++ js/buscar.js
@@ -1,18 +1,38 @@
-async function buscar(){
+async function buscar() {
     console.log("Passei por aqui");
 
-    const resposta = await fetch(`${process.env.API_URL}`);
+    const resposta = await fetch("https://660f44b2356b87a55c510de7.mockapi.io/agendas");
     const respostaDadosAgenda = await resposta.json();
 
     console.log("Resposta: ", respostaDadosAgenda);
 
-   const cards = document.getElementById("cards_games");
-   
+    const cards = document.getElementById("cards_games");
+
     cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
-           return "TESTE "
-    });
+        return `
+        <div class="cardItem">
+        <div class="dataGame">
+            <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo}
+        </div>
+        <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+        <div class="descricao">
+            <p> <strong>${itemAgenda.nome}</strong> </p>
+            <p> ${itemAgenda.descricao} </p>
+            <p><strong>Gamers: </strong></p>
+
+            <div class="gamers">
+            ${itemAgenda.gamers.map((gamerItem) => {
+            return `<div class="gamerItem">@${gamerItem}</div>`
+        }).join("")
+            }
+            </div>
+        </div>
+        <div class="assistir">
+            <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+        </div>
+    </div>
+        `
+    }).join("");
 }
 
-console.log("antes de buscar")
 buscar();
-console.log("depois de buscar")
